b00ab8253369a682b83bee1f6c3c6b88fa9eec61
Luca Giozza <luca.giozza@gmail.com>
2021-05-26 17:03:42 +0200

    ultime correzioni file js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var nome = prompt('Inserisci il tuo nome');
-testo = parseInt(nome);
+
 console.log(nome);
 
 var cognome = prompt('Inserisci il tuo cognome');
- testo = parseInt(cognome);
+ 
 console.log(cognome);
 
 var colore = prompt('Inserisci il tuo colore preferito');
-testo = parseInt(colore);
+
 console.log(colore);
 
 document.getElementById('password').innerHTML=nome + cognome + colore
